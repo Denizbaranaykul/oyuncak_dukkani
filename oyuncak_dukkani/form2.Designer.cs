89495426061b19f8e06c38af973f3b7faa1144a5
AHMETBNL <abenli24@posta.pau.edu.tr>
2025-05-12 23:05:00 +0300

    AAAAAAAAAA

---
+++ oyuncak_dukkani/form2.Designer.cs
@@ -0,0 +1,409 @@
+﻿namespace oyuncak_dukkani
+{
+    partial class form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            pictureBox11 = new PictureBox();
+            pictureBox12 = new PictureBox();
+            pictureBox13 = new PictureBox();
+            pictureBox14 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(135, 98);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(109, 100);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(301, 98);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(109, 100);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(474, 98);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(109, 100);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(635, 98);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(109, 96);
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(798, 98);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(109, 100);
+            pictureBox5.TabIndex = 4;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(966, 98);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(109, 100);
+            pictureBox6.TabIndex = 5;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Location = new Point(1119, 98);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(109, 100);
+            pictureBox7.TabIndex = 6;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Location = new Point(135, 310);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(109, 100);
+            pictureBox8.TabIndex = 7;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Location = new Point(301, 310);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(109, 100);
+            pictureBox9.TabIndex = 8;
+            pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.Location = new Point(474, 310);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(109, 100);
+            pictureBox10.TabIndex = 9;
+            pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            pictureBox11.Location = new Point(635, 310);
+            pictureBox11.Name = "pictureBox11";
+            pictureBox11.Size = new Size(109, 100);
+            pictureBox11.TabIndex = 10;
+            pictureBox11.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            pictureBox12.Location = new Point(798, 310);
+            pictureBox12.Name = "pictureBox12";
+            pictureBox12.Size = new Size(109, 100);
+            pictureBox12.TabIndex = 11;
+            pictureBox12.TabStop = false;
+            // 
+            // pictureBox13
+            // 
+            pictureBox13.Location = new Point(966, 310);
+            pictureBox13.Name = "pictureBox13";
+            pictureBox13.Size = new Size(109, 100);
+            pictureBox13.TabIndex = 12;
+            pictureBox13.TabStop = false;
+            // 
+            // pictureBox14
+            // 
+            pictureBox14.Location = new Point(1119, 310);
+            pictureBox14.Name = "pictureBox14";
+            pictureBox14.Size = new Size(109, 100);
+            pictureBox14.TabIndex = 13;
+            pictureBox14.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(135, 230);
+            button1.Name = "button1";
+            button1.Size = new Size(109, 29);
+            button1.TabIndex = 14;
+            button1.Text = "sepete ekle";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(301, 230);
+            button2.Name = "button2";
+            button2.Size = new Size(109, 29);
+            button2.TabIndex = 15;
+            button2.Text = "sepete ekle";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(474, 230);
+            button3.Name = "button3";
+            button3.Size = new Size(109, 29);
+            button3.TabIndex = 16;
+            button3.Text = "sepete ekle";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(635, 230);
+            button4.Name = "button4";
+            button4.Size = new Size(109, 29);
+            button4.TabIndex = 17;
+            button4.Text = "sepete ekle";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(798, 230);
+            button5.Name = "button5";
+            button5.Size = new Size(109, 29);
+            button5.TabIndex = 18;
+            button5.Text = "sepete ekle";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(966, 230);
+            button6.Name = "button6";
+            button6.Size = new Size(109, 29);
+            button6.TabIndex = 19;
+            button6.Text = "sepete ekle";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(1119, 230);
+            button7.Name = "button7";
+            button7.Size = new Size(109, 29);
+            button7.TabIndex = 20;
+            button7.Text = "sepete ekle";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(135, 436);
+            button8.Name = "button8";
+            button8.Size = new Size(109, 29);
+            button8.TabIndex = 21;
+            button8.Text = "sepete ekle";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(301, 436);
+            button9.Name = "button9";
+            button9.Size = new Size(109, 29);
+            button9.TabIndex = 22;
+            button9.Text = "sepete ekle";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(474, 436);
+            button10.Name = "button10";
+            button10.Size = new Size(109, 29);
+            button10.TabIndex = 23;
+            button10.Text = "sepete ekle";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(635, 436);
+            button11.Name = "button11";
+            button11.Size = new Size(109, 29);
+            button11.TabIndex = 24;
+            button11.Text = "sepete ekle";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(798, 436);
+            button12.Name = "button12";
+            button12.Size = new Size(109, 29);
+            button12.TabIndex = 25;
+            button12.Text = "sepete ekle";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(966, 436);
+            button13.Name = "button13";
+            button13.Size = new Size(109, 29);
+            button13.TabIndex = 26;
+            button13.Text = "sepete ekle";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(1119, 436);
+            button14.Name = "button14";
+            button14.Size = new Size(109, 29);
+            button14.TabIndex = 27;
+            button14.Text = "sepete ekle";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(1329, 98);
+            button15.Name = "button15";
+            button15.Size = new Size(128, 29);
+            button15.TabIndex = 28;
+            button15.Text = "sepeti görüntüle";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1515, 772);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox14);
+            Controls.Add(pictureBox13);
+            Controls.Add(pictureBox12);
+            Controls.Add(pictureBox11);
+            Controls.Add(pictureBox10);
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "form2";
+            Text = "form2";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox10;
+        private PictureBox pictureBox11;
+        private PictureBox pictureBox12;
+        private PictureBox pictureBox13;
+        private PictureBox pictureBox14;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+    }
+}
